9940fd52b01f3f49d0c9ccaedc1524fe560c45eb
Pixel <110355772+EluminousPixel@users.noreply.github.com>
2022-08-02 16:14:19 +0100

    Create Higher_Lower.java

---
+++ Higher_Lower.java
@@ -0,0 +1,36 @@
+package Higher_lower;
+import java.util.Scanner;
+
+public class Higher_Lower {
+    
+
+    public static void main(String[] args) {
+
+        while (true) {
+            
+            int ranNumber = (int) (Math.random() * 100);
+            int ranNumber2 = (int) (Math.random() * 100);
+            
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.println("Welcome to higher or lower" );
+            System.out.println("Is the next number to " + ranNumber + " going to be higher or lower "); 
+            String userInput = scanner.next(); {
+            
+            if (userInput.equals("lower") && ranNumber >= ranNumber2) 
+            System.out.println("Correct :D It was " + ranNumber2 + " which is lower");
+
+            if (userInput.equals("higher") && ranNumber <= ranNumber2) 
+            System.out.println("Correct :D It was " + ranNumber2 + " which is higher");
+
+            else if (userInput.equals("higher") && ranNumber >= ranNumber2) 
+            System.out.println("Wrong D: It was " + ranNumber2 + " which is lower");
+            
+            else if (userInput.equals("lower") && ranNumber <= ranNumber2) 
+            System.out.println("Wrong D: It was " + ranNumber2 + " which is higher");
+            }
+
+        }
+
+    }
+}
